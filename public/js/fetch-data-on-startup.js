97d0ece9b8530a86b57d0766ce4a9b81b66fcabc
Dany King <danielkkoper@gmail.com>
2023-02-01 15:50:04 +0100

    update deleted/moved or rename files

--- public/js/fetch-data-on-startup.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  try {
-    const res = await fetch('http://127.0.0.1:3000/saved');
-    const data = await res.json();
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-})();
