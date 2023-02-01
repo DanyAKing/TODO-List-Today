486f06f30d31b5b6eb32d36decd617bf9bf60ed1
Dany King <danielkkoper@gmail.com>
2023-02-01 03:05:55 +0100

    working on functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,79 @@
 const addBtn = document.querySelector('#add_btn');
 const inputTask = document.querySelector('#input_task');
-const tasks = document.querySelector('#tasks');
+const tasks = document.querySelector('.tasks');
 
-const simpleTaskCreator = inputData => {
-  const task = document.createElement('p');
-  task.innerText = inputData;
-  return task;
-};
 
-addBtn.addEventListener('click', async () => {
+const changeInputPlaceholder = (constructor) => {
   if (inputTask.value.length === 0) {
     inputTask.setAttribute('placeholder', 'wprowadź zadanie do zrealizowania!');
     setTimeout(() => {
       inputTask.setAttribute('placeholder', 'zadanie do zrealizowania...');
-    }, 2500);
+    }, 2200);
   } else {
-    tasks.appendChild(simpleTaskCreator(inputTask.value));
+    tasks.appendChild(constructor);
   }
+};
+
+const buttonCreator = (idName, nameButton, target) => {
+  const btn = document.createElement('button');
+  btn.setAttribute('id', idName);
+  btn.setAttribute('type', 'button');
+  btn.innerText = nameButton;
+
+  target.appendChild(btn);
+  return target;
+};
+
+const singleTaskCreator = (inputData) => {
+  const contener = document.createElement('div');
+  contener.className = 'task';
+
+  const textContener = document.createElement('span');
+  textContener.className = 'text_contener';
+  const buttonContener = document.createElement('span');
+  buttonContener.className = 'button_contener';
+
+  textContener.className = 'textContener';
+  textContener.innerText = inputData;
+  contener.appendChild(textContener);
+
+  contener.appendChild(buttonCreator('remove_btn', 'Usuń', buttonContener));
+  buttonCreator('edit_btn', 'Edytuj', buttonContener);
+  
+  return contener;
+};
+
+const sendDataToBackend = async () => {
+  await fetch('http://127.0.0.1:3000/task', {
+    method: 'POST',
+    body: JSON.stringify({
+      content: inputTask.value,
+      done: false,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  }).then(res => res.json())
+    .then(data => {
+      const { content } = data;
+      singleTaskCreator(content);
+    });
+};
+
+// const fetchDataOnStartup = async () => {
+//   try {
+//     const res = await fetch('http://127.0.0.1:3000/saved');
+//     const data = await res.json();
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// fetchDataOnStartup();
+
+addBtn.addEventListener('click', async event => {
+  event.preventDefault();
+  changeInputPlaceholder(singleTaskCreator(inputTask.value));
+  sendDataToBackend();
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { writeFile, readFile } = require('fs').promises;
+const { taskStorage } = require('../src/task-storage');
 
 const router = express.Router();
+const FILE_PATH = './database/data.json';
 
 router
-  .get('/', (req, res) => {
-    console.log(`Visit ${req.path}`);
+  .post('/task', async (req, res) => {
+    taskStorage.tasks.push(req.body);
+    await writeFile(FILE_PATH, JSON.stringify(taskStorage), 'utf-8');
+    console.log(taskStorage.tasks);
+
+    res.json(req.body);
+  })
+  .get('/saved', async (req, res) => {
+    const savedData = await readFile(FILE_PATH, 'utf-8');
+    res.json(savedData);
   });
 
 module.exports = { router };
