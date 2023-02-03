84639aa2a907f851cb56067f213e7fe403fa82a6
Dany King <danielkkoper@gmail.com>
2023-02-03 14:53:13 +0100

    remove forEach loops, change for copy entire object and use try/catch for display information in backend

--- src/fetch-data-on-startup.js
+++ src/fetch-data-on-startup.js
@@ -2,11 +2,15 @@ const { readFile } = require('fs').promises;
 
 const fetchDataOnStartup = async (filePath, database) => {
   const res = await readFile(filePath, 'utf-8');
-  const data = JSON.parse(res);
-  data.tasks.forEach(el => {
-    database.tasks.push(el);
-  });
-  console.log(database);
+  try {
+    const data = JSON.parse(res);
+    Object.assign(database, data);
+
+    console.log('Restored last session:');
+    console.log(database);
+  } catch (e) {
+    console.error('Database is empty.', e.message);
+  }
 };
 
 module.exports = { fetchDataOnStartup };
