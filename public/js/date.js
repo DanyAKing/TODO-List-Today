80455688af89d8777856d85b6aa18c55733c93cc
Dany King <danielkkoper@gmail.com>
2023-02-05 22:39:52 +0100

    correct months name in object

--- public/js/date.js
+++ public/js/date.js
@@ -1,16 +1,16 @@
 const plMonth = {
-  1: 'stycznia',
-  2: 'lutego',
-  3: 'marca',
-  4: 'kwietnia',
-  5: 'maja',
-  6: 'czerwca',
-  7: 'lipca',
-  8: 'sierpnia',
-  9: 'września',
-  10: 'października',
-  11: 'listopada',
-  12: 'grudnia',
+  1: 'styczeń',
+  2: 'luty',
+  3: 'marzec',
+  4: 'kwiecień',
+  5: 'maj',
+  6: 'czerwiec',
+  7: 'lipiec',
+  8: 'sierpień',
+  9: 'wrześień',
+  10: 'październik',
+  11: 'listopad',
+  12: 'grudzień',
 };
 
 const getDate = () => {
