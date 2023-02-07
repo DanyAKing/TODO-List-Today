0f66ce27957965d697b37c5ad2156ef5f34c1807
Dany King <danielkkoper@gmail.com>
2023-02-07 16:28:15 +0100

    remove Object.assign to scale both object (data, database), added array methods to copy from data to database

--- src/fetch-data-on-startup.js
+++ src/fetch-data-on-startup.js
@@ -1,13 +1,15 @@
 const { readFile } = require('fs').promises;
+const { taskStorage } = require('./task-storage');
 
-const fetchDataOnStartup = async (filePath, database) => {
+const fetchDataOnStartup = async (filePath) => {
   const res = await readFile(filePath, 'utf-8');
   try {
     const data = JSON.parse(res);
-    Object.assign(database, data);
+    const filteredData = data.tasks.filter(item => item.done !== true);
+    taskStorage.tasks = filteredData;
 
     console.log('Restored last session:');
-    console.log(database);
+    console.log(taskStorage);
   } catch (e) {
     console.error('Database is empty.', e.message);
   }
