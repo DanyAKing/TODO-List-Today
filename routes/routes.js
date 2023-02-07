ab49872db59c7b57a35406ad9c4b26b710ae13dc
Dany King <danielkkoper@gmail.com>
2023-02-07 16:36:17 +0100

    refractorization routes for backend

--- routes/routes.js
+++ routes/routes.js
@@ -6,39 +6,59 @@ const { fetchDataOnStartup } = require('../src/fetch-data-on-startup');
 const router = express.Router();
 const FILE_PATH = './database/data.json';
 
-fetchDataOnStartup(FILE_PATH, taskStorage);
+const saveData = async () => {
+  await writeFile(FILE_PATH, JSON.stringify(taskStorage), 'utf-8');
+};
+const response = (res) => res.status(200).json({ itemCounter, taskStorage });
+
+fetchDataOnStartup(FILE_PATH, taskStorage.tasks);
 const itemCounter = 0;
 
 router
   .post('/task', async (req, res) => {
     taskStorage.tasks.push(req.body);
-    await writeFile(FILE_PATH, JSON.stringify(taskStorage), 'utf-8');
 
-    console.log('Added:');
-    console.log(req.body);
+    saveData();
+    res.sendStatus(200);
 
-    res.json(itemCounter, taskStorage);
+    console.log('Added task:');
+    console.log(taskStorage);
   })
   .get('/remove/all', async (req, res) => {
     taskStorage.tasks.splice(0, taskStorage.tasks.length);
-    await writeFile(FILE_PATH, JSON.stringify(taskStorage), 'utf-8');
-    res.json(true);
-    console.log('Removed list.');
+
+    saveData();
+    res.status(200).json(true);
+
+    console.log('Remove all list.');
   })
   .post('/remove/item', async (req, res) => {
     const { element } = req.body;
-    taskStorage.tasks.splice(element - 1, 1);
-    await writeFile(FILE_PATH, JSON.stringify(taskStorage), 'utf-8');
-    res.json(200, itemCounter, taskStorage);
-  })
-  .get('/refresh', async (req, res) => {
-    res.json(200, itemCounter, taskStorage);
+    taskStorage.tasks.splice(element, 1);
+
+    saveData();
+    response(res);
+
+    console.log('Remove item:');
+    console.log(taskStorage);
   })
   .get('/saved', async (req, res) => {
-    res.json({ itemCounter, taskStorage });
+    response(res);
   })
   .post('/done', async (req, res) => {
-    console.log(req.body);
+    const { done, id } = req.body;
+    taskStorage.tasks[id].done = done;
+
+    console.log('Change done status:');
+    console.log(taskStorage);
+
+    saveData();
+    res.sendStatus(200);
+  })
+  .post('/refresh', async (req, res) => {
+    const { refresh } = req.body;
+    if (refresh === true) taskStorage.tasks.filter(item => item.done !== true);
+    response(res);
   });
 
 module.exports = { router, FILE_PATH };
